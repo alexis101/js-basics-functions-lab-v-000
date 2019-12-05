53f804ffaba62bde973b616b48ff76f1f23c9104
Alexis Kondilis <kondilisalexis@gmail.com>
2019-12-05 15:08:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ function distanceFromHqInBlocks(blocks){
    return distanceFromHqInBlocks(blocks) * 264
  }
 
- function distanceTravelledInFeet(aBlock, bBlock){
-   return `${(aBlock - bBlock)*264}`
+ function distanceTravelledInFeet(blocks, 42){
+   if (blocks > 42){
+     return `${(blocks - 42)* 264}`
+   }
+   else {
+     return `${(42 - blocks)* 264}`
+   }
  }
